fc5ecf989dbee22dd82f7a12d71391ca4bb43f73
michepriest <miche.priest@gmail.com>
2018-10-02 06:09:30 +0000

    added callback functions to get and print data

--- getting-with-parameters-3.js
+++ getting-with-parameters-3.js
@@ -4,7 +4,7 @@ let requestOptions = {
   path: "/http-examples/step2.html"
  };
 
-function getHTML (options) {
+function getAndPrintHTML (options) {
 
   let buffedData = ""; // anytime you have an empty variable you're going to change the variable so use "let"
 
@@ -19,5 +19,29 @@ function getHTML (options) {
     });
   });
 }
-getHTML(requestOptions);
+
+function getHTML(options, callback) {
+  let buffedData = ""; // anytime you have an empty variable you're going to change the variable so use "let"
+
+   https.get(options, function (response) {
+    response.setEncoding("utf8");
+    response.on("data", function (data) {
+      buffedData += data;
+    })
+    response.on("end", function() {
+      // console.log the data once all of the data has been received
+      callback(buffedData);
+    });
+  });
+}
+
+getHTML(requestOptions, printHTML)
+// The function body will invoke (call) the callback when the data is fully received, 
+// in place of the original console.log
+
+function printHTML (html) {
+  console.log(html);
+}
+
+getAndPrintHTML(requestOptions);
 
